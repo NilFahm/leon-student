6b29d5b91593a7354d0e0a3b761cb18a5e8fd0d8
Nil Mistry <nil.fahm@gmail.com>
2022-05-30 15:48:35 +0530

    Changes

--- src/data/Config.js
+++
@@ -1,3 +0,0 @@
-export const Config = {
-  baseUrl: "https://leon-api.equitec.in",
-};
--- src/pages/Pages.js
+++
@@ -1,5 +0,0 @@
-import Login from "./Login";
-import Schedules from "./Schedules";
-import StartCall from "./StartCall";
-import Classroom from "./Classroom";
-export const Pages = () => ({ Login, Schedules, StartCall, Classroom });
